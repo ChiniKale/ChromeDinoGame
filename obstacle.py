ba4c6410ebdd83088f93533e0f2bb355b092b57c
ChiniKale <chinmaykale1@gmail.com>
2024-11-18 01:17:51 +0530

    birds flap now!!

--- obstacle.py
+++ obstacle.py
@@ -8,16 +8,27 @@ class Obstacle:
         self.x = x
         self.size = size
         self.GroundHeight = GroundHeight
+        self.current_frame = 0
+        self.animation_time = 0.5  # Time per frame in seconds
+        self.time_accumulator = 0 
         if is_high:
-            self.y = GroundHeight - size - 30  # Height of the sky obstacles
+            self.y = GroundHeight - size - 30
+            self.frames = [pygame.transform.scale(pygame.image.load(r"birb.png"), (size, size)), pygame.transform.scale(pygame.image.load(r"birb2.png"), (size, size))]  # Height of the sky obstacles
         else:
-            self.y = GroundHeight - size  # Ground obstacles
+            self.y = GroundHeight - size
+            self.frames = [pygame.transform.scale(pygame.image.load(r"cactus.png"), (size, size)), pygame.transform.scale(pygame.image.load(r"cactus.png"), (size, size))]  # Ground obstacles
 
+    
     def draw(self, gameDisplay):
-        pygame.draw.rect(gameDisplay, colour, [self.x, self.y, self.size, self.size])
+        current_image = self.frames[self.current_frame % len(self.frames)]
+        gameDisplay.blit(current_image, (self.x, self.y))
     
     def update(self, deltaTime, velocity):
         self.x -= velocity * deltaTime
+        self.time_accumulator += deltaTime
+        if self.time_accumulator > self.animation_time:
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.time_accumulator = 0   
 
     def checkOver(self):
         return self.x < 0
