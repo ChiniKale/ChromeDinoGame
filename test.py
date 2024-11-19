d57d8bd0aae51bf33245c6539dd569a6bacb3a7e
ramanan849 <rgokularamanan@gmail.com>
2024-11-19 09:53:18 +0000

    test-update

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 print("Test file")
 print("Finally working!!!!!! - lukog, 18/11/2024")
+# Recomputing the series using the given formula
+series_sum = sum((1 / (1.07 ** n)) for n in range(0, 51))  # Series summation
+total_value = 20000 * series_sum  # Final value computation
+
+print(total_value)
+
+# yo this bgm rocks!!
